e9280a2613b89fb392ffcf68213cd7936ace7f1b
hugomatijascic <hugo.matijascic@polymtl.ca>
2020-09-14 18:17:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return (angle_degs + angle_mins/60 + angle_secs/3600) * math.pi)/180 
+    return lat(angle_degs + angle_mins/60 + angle_secs/3600) * math.pi)/180 
 
 
 def to_degrees(angle_rads: float) -> tuple:
